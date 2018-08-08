0c694879a3fd2a347ffa3a59a38ed970535f7443
Sav Younts <savyounts@gmail.com>
2018-08-08 19:52:19 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
   end 
   
   def add_item(title, price, quantity = 1)
-    @item_list << title
+    self.item_list << title
     price_increase = price * quantity
     @total = @total + price_increase
     @total 
